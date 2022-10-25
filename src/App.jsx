e4dc79c86607882ccabb9437d87b39733fde188e
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-25 22:00:50 +0530

    components import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './style'
 
+import{  Navbar, Billing, Business, Button,  CardDeal, Clients,CTA, FeedbackCard,Footer, GetStarted,  Hero,
+  Stats, Testimonials,} from './components'
+
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
@@ -11,20 +14,13 @@ const App = () => {
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero/>
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats 
-          Business 
-          Billing 
-          CardDeal 
-          Testimonials 
-          Clients 
-          CTA 
-          Footer 
+         
         </div>
       </div>
 
