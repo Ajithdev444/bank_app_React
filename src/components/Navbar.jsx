5af97ce76372a2f1d0716ea35962ce952a731290
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-26 08:21:23 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
+
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
+  
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]'/>
+
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, index) =>(
+          <li
+          key={nav.id}
+          className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1
+             ? 'mr-0' : 'mr-10'} text-white `}
+          >
+            <a href={`${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+
+      </div>
+
+    </nav>
   )
 }
 
