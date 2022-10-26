9d7acd67e5607dca1815990f3ea0647d852f6e22
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-26 17:35:01 +0530

    stats

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,14 @@ const App = () => {
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-         
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
         </div>
       </div>
 
