0c4eccae88a33150b83f24907d49a3e499ef2f68
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-26 16:31:35 +0530

    Hero robo

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { arrowUp } from '../assets'
+import styles from '../style'
 
 const GetStarted = () =>  (
     <div  className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient
