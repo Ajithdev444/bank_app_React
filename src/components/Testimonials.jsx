19275d6f45215f1359ceb150097e4ef1b58eff33
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-27 11:28:25 +0530

    peoples saying

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Feedback } from '../constants'
+import styles from '../style'
+import { feedback } from '../constants'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id='clients' className={`${styles.paddingY} ${styles.flexCenter}
+    flex-col relative`}>
+      <div/>
+
+      <div className='w-full flex justify-between items-center
+      md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+        <h1 className={`${styles.heading2}`}>What people are
+        <br className='sm:block hidden'/> sayind about us</h1>
+          <div className='w-full md:mt-0 mt-6'>
+            <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+              Everythind you need to accept card payments and grow your 
+              business any where on the planet.
+            </p>
+          </div>
+      </div>
+
+    </section>
   )
 }
 
