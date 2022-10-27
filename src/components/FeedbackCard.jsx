22177db48dc90cda0c3c02415414cc6bc03c3de8
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-27 12:03:15 +0530

    Testimonials and FeedbackCard

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { quotes } from '../assets'
 
-const FeedbackCard = () => {
+const FeedbackCard = ({ content, title, name, img }) => {
   return (
-    <div>FeedbackCard</div>
+    <div className='flex flex-col justify-between px-10 py-12
+    rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 feedback-card'>
+      <img src={quotes} alt='double_quotes' className='w-[42px]
+      h-[27px] object-contain'/>
+      <p className='font-poppins font-normal my-10 text-white
+       text-[18px] leading-[32px]'>{content}</p>
+
+       <div className='flex flex-row'>
+        <img src={img} alt={name} className='w-[48px] h-[48px]
+        rounded-full' />
+        <div className='flex flex-col ml-4'>
+          <h4 className='font-poppins font-semibold text-white
+       text-[20px] leading-[32px]'>{name}</h4>
+          <p className='font-poppins font-normal text-dimwhite
+       text-[16px] leading-[24px]'>{title}</p>
+        </div>
+       </div>
+
+    </div>
   )
 }
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Feedback } from '../constants'
+import { FeedbackCard } from './FeedbackCard'
 import styles from '../style'
 import { feedback } from '../constants'
 
@@ -7,7 +7,8 @@ const Testimonials = () => {
   return (
     <section id='clients' className={`${styles.paddingY} ${styles.flexCenter}
     flex-col relative`}>
-      <div/>
+      <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%]
+      rounded-full blue__gradient'/>
 
       <div className='w-full flex justify-between items-center
       md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
@@ -19,6 +20,14 @@ const Testimonials = () => {
               business any where on the planet.
             </p>
           </div>
+      </div>
+
+      <div className='flex flex-wrap sm:justify-start justify-center
+      w-full feedback-container relative z-[1]'>
+        {feedback.map((card) => (
+            <FeedbackCard key={card.id} {...card} />
+        ))} 
+
       </div>
 
     </section>
